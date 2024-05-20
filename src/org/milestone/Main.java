0677de418971d990e4590ba44e1eac0b806d047b
Dariya Kashtan <kashtandariya@gmail.com>
2024-05-20 20:56:18 +0200

    inizio milestone
    Pagina di main e pagina di commenti con istruzioni iniziali che andrò ad aggiornare con l'avanzamento dell'esercizio

---
+++ src/org/milestone/Main.java
@@ -0,0 +1,5 @@
+package org.milestone;
+
+public class Main {
+	
+}
---
+++ src/org/milestone/Notes.java
@@ -0,0 +1,28 @@
+/*Stiamo lavorando a un programma che deve gestire eventi (ad esempio concerti, 
+conferenze, spettacoli etc.).
+ 
+
+1. Creare una classe Evento che abbia le seguenti proprietà:
+	-  titolo -  data -  numero di posti in totale -  numero di posti prenotati 
+--> Quando si istanzia un nuovo evento questi attributi devono essere tutti valorizzati nel 
+costruttore, tranne posti prenotati che va inizializzato a 0. 
+
+2. Inserire il controllo che la data non sia già passata e che il numero di posti totali sia positivo. 
+--> In caso contrario mostrare i dovuti avvisi all’utente 
+
+3 Aggiungere metodi getter e setter in modo che:
+- titolo/data sia in lettura e in scrittura 
+- numero di posti totale e prenotati siano solo in lettura 
+
+4. Vanno inoltre implementati dei metodi public che svolgono le seguenti funzioni: 
+-  prenota:  aggiunge uno ai posti prenotati. Se l’evento  è già passato o non ha posti 
+disponibili deve restituire un messaggio di avviso. 
+
+-  disdici  : riduce di uno i posti prenotati. Se l’evento  è già passato o non ci sono 
+prenotazioni restituisce un messaggio di avviso. 
+
+-  l’override del metodo toString()  in modo che venga  restituita una stringa contenente: 
+data formattata - titolo 
+Aggiungete eventuali metodi (public e private) che vi aiutino a svolgere le funzioni richieste.
+
+*/
